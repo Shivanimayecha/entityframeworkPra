ae5ff61600911fc8573c51a597415230a6334ec5
Shivani <shivanimayecha0908@gmail.com>
2019-06-11 00:17:54 +0530

    Add project files.

---
+++ entityframeworkPra/EFPractical.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EFPractical.aspx.cs" Inherits="entityframeworkPra.EFPractical" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <%--    <div>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <asp:TextBox runat="server" ID="txtId"></asp:TextBox>
+            <input type="text" placeholder="Id" name="id" id="txtId"/>
+
+        </div>--%>
+        <br />
+        <br />
+        <div>
+            <asp:Label ID="lbl_name" runat="server" Text="Name :"></asp:Label>
+            <asp:TextBox runat="server" ID="txtName"></asp:TextBox>
+            <%--<input type="text" placeholder="Name" name="Name" id="txtName" />--%>
+        </div>
+        <br />
+        <br />
+        <div>
+            <asp:Label ID="lbl_cname" runat="server" Text="CompanyName :"></asp:Label>
+
+            <asp:TextBox runat="server" ID="txtCname">
+            </asp:TextBox>
+            <%--            <input type="text" placeholder="CompanyName" name="CompanyName" id="txtCname" />--%>
+        </div>
+        <br />
+        <br />
+        <div>
+            <asp:Label ID="lbl_Add" runat="server" Text="Address :"></asp:Label>
+
+            <asp:TextBox runat="server" ID="txtAdd" TextMode="MultiLine"></asp:TextBox>
+            <%--            <input type="text" placeholder="Address" name="Address" id="txtAdd" />--%>
+        </div>
+        <br />
+        <br />
+        <div>
+            <asp:Button ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" Text="Submit" />
+            <asp:Button ID="btnUpdate" runat="server" OnClick="btnUpdate_Click" Text="Update" Height="26px" Style="margin-left: 17px" Width="91px" />
+            <asp:Button ID="btnDelete" runat="server" OnClick="btnDelete_Click" Text="Delete" Style="margin-left: 16px" Width="79px" />
+        </div>
+        <br />
+
+        <div>
+            <asp:GridView ID="gridView" runat="server">
+            </asp:GridView>
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ entityframeworkPra/EFPractical.aspx.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+using System.Web.UI.WebControls;
+
+
+namespace entityframeworkPra
+{
+    public partial class EFPractical : System.Web.UI.Page
+    {
+        //demoEntities _dbObj;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //_dbObj = new demoEntities();
+
+            BindGrid();
+
+
+
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+
+            using (demoEntities entities = new demoEntities())
+            {
+                employee emp = new employee();
+                emp.Name = txtName.Text;
+                emp.CompanyName = txtCname.Text;
+                emp.Address = txtAdd.Text;
+
+                entities.employees.Add(emp);
+                entities.SaveChanges();
+                Response.Write("<script>alert('Inserted Successfully..');</script>");
+                BindGrid();
+            }
+
+
+
+
+            //employee emp = new employee();
+            //emp.Name = txtName.Text;
+            //emp.CompanyName = txtCname.Text;
+            //emp.Address = txtAdd.Text;
+
+            //_dbObj.employees.Add(emp);
+            //_dbObj.SaveChanges();
+
+            //Response.Write("<script>alert('Inserted Successfully..');</script>");
+
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            //HERE I JUST CHANGE COMAPNAYNAME OF EMPLOYEE
+
+            string empuser = txtName.Text;
+            using (demoEntities entities = new demoEntities())
+            {
+                employee emp = entities.employees.First(i => i.Name == txtName.Text);
+                emp.CompanyName = txtCname.Text;
+
+                entities.SaveChanges();
+                Response.Write("<script>alert('Updated Successfully..');</script>");
+                BindGrid();
+
+            }
+        }
+
+            protected void btnDelete_Click(object sender, EventArgs e)
+            {
+                string empuser = txtName.Text;
+                using (demoEntities entities = new demoEntities())
+                {
+                    employee emp = entities.employees.First(i => i.Name == txtName.Text);
+                    entities.employees.Remove(emp);
+
+                    entities.SaveChanges();
+                    Response.Write("<script>alert('Deleted Successfully..');</script>");
+                    BindGrid();
+
+                }
+            }
+
+            private void BindGrid()
+            {
+                using (demoEntities entities = new demoEntities())
+                {
+
+                gridView.DataSource = entities.employees.ToList<employee>();
+                gridView.DataBind();
+                //var load = from g in entities.employees select g;
+                //if (load != null)
+                //{
+                //    gridView.DataSource = load.ToList();
+                //    gridView.DataBind();
+                //}
+            }
+            }
+        }
+    }
